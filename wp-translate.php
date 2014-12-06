860c3294f1f710e03615963da4e387f2b71267ed
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-06 22:34:45 +0000

    update version 4.7 - 1) added setting to include/exclude mobile browsers. 2) added setting to change GT toolbar autoDisplay property.
    git-svn-id: https://plugins.svn.wordpress.org/wp-translate/trunk@1039630 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-translate.php
+++ wp-translate.php
@@ -4,29 +4,56 @@ Plugin Name: WP Translate
 Plugin URI: http://labs.hahncreativegroup.com/wp-translate-pro/
 Description: Add Google Translate to your WordPress site
 Author: HahnCreativeGroup
-Version: 4.6
+Version: 4.7
 Author URI: http://labs.hahncreativegroup.com/google-translate-wordpress-plugin/
 */
 
 register_activation_hook( __FILE__,  'wpTranslate_install' );
 
 function wpTranslate_install() {
+	if(!get_option("wpTranslateOptions")) {
 	$wpTranslateOptions = array(
 								"default_language" => "auto",
 								"tracking_enabled" => false,
-								"tracking_id" => ""
+								"tracking_id" => "", 
+								"auto_display" => true,
+								"exclude_mobile" => true
 								);
 	
 	add_option("wpTranslateOptions", $wpTranslateOptions);
+	}
+	else {
+		$wpTranlsateOptions	= get_option("wpTranslateOptions");
+		$keys = array_keys($wpTranlsateOptions);		
+		
+		if (!in_array('default_language', $keys)) {
+			$wpTranlsateOptions['default_language'] = "auto";	
+		}
+		if (!in_array('auto_display', $keys)) {
+			$wpTranlsateOptions['auto_display'] = true;	
+		}
+		if (!in_array('exclude_mobile', $keys)) {
+			$wpTranlsateOptions['exclude_mobile'] = true;	
+		}
+		
+		update_option("wpTranslateOptions", $wpTranlsateOptions);
+	}
 }
 
 function translate_Init() {
-	$wpTranslateOptions = get_option(WPTRANSLATEOPTIONS);
+	$wpTranslateOptions = get_option("wpTranslateOptions");
+	$doTranslate = true;
+	if ($wpTranslateOptions["exclude_mobile"]) {		
+		$agent = $_SERVER['HTTP_USER_AGENT'];
+		if(preg_match('/iPhone|Android|Blackberry|Windows Phone/i', $agent)){
+			$doTranslate = false;
+		}
+	}
 	$agent = $_SERVER['HTTP_USER_AGENT'];  
-if(!preg_match('/iPhone|Android|Blackberry|Windows Phone/i', $agent)){
+if($doTranslate){
 	?>
 	<!-- WP Translate - Google Translate: http://labs.hahncreativegroup.com/google-translate-wordpress-plugin/ -->
-    <script>
+    <script type='text/javascript'>
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
     pageLanguage: '<?php echo $wpTranslateOptions["default_language"]; ?>',
@@ -34,7 +61,8 @@ function googleTranslateElementInit() {
 	gaTrack: true,
     gaId: '<?php echo $wpTranslateOptions["tracking_id"]; ?>',
 	<?php } ?>
-    floatPosition: google.translate.TranslateElement.FloatPosition.TOP_RIGHT
+    floatPosition: google.translate.TranslateElement.FloatPosition.TOP_RIGHT,
+	autoDisplay: <?php echo ($wpTranslateOptions["auto_display"]) ? "true" : "false"; ?>
   });
 }
 </script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
